51b679004c000fc62d952b2e826cf57dbecd53fb
Ekena <ekenarakotoson02@gmail.com>
2024-10-22 11:35:20 +0300

    tout dans la classe

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-class Questionnaires:
-    def __init__(self,question,liste_reponses,reponse):
-        self.question = question
-        self.liste_reponse = liste_reponses
-        self.reponse = reponse
-        self.poser_question(self.question, self.liste_reponse,self.reponse)
+from random import shuffle
 
+
+class Questionnaires:
+    datas = [("quel est la capitale de Madagascar ?", ['Fianarantsoa', 'Toamasina', 'Antananarivo', 'Toliara'],
+              'Antananarivo'),
+             ("Qui est le président de la republique à Madagascar ?",
+              ['Andry Rajoelina', 'Philibert Tsiranana', 'Donald Trump', 'Marc Ravalomanana'], 'Andry Rajoelina'),
+             ("trouvez l'intru...", ['oeil', 'dent', "langue", "peau", 'oreil', 'nez'], 'dent'),
+             ("quel esr le contraire de \"inflammable\" ?",
+              ["flammable", "ininflammable", "imperméable", "ininflammable", "incombustible"], "incombustible"),
+             ("en quel année le Stephen Hawking est décédé ?", ["2018", "2015", "2013", "2020"], "2018"),
+             ("dans quel équipe Paul Pogba n'a pas joué", ["Juventus", "Manchester United", "Chelsea"], "Chelsea"),
+             ("laquelle de ces maladies est incurable ?", ["SIDA", 'Tuberculose', "Peste", "Choléra", "Corona virus"],
+              "SIDA")]
+
+    def __init__(self):
+        self.stop = False
+        shuffle(self.datas)
+        for data in self.datas:
+            if not self.stop:
+                self.poser_question(data[0], data[1],data[2])
     def poser_question(self,question,liste_reponses,reponse):
+        shuffle(liste_reponses)
         num_reponse = liste_reponses.index(reponse)
-        print(question)
+        print(question.title())
         i = 1
         for item in liste_reponses:
             print(f"\t{i} - {item}")
@@ -23,8 +39,20 @@ class Questionnaires:
 
 
 
-datas = [("quel est la capitale de Madagascar ?",('Fianarantsoa','Toamasina','Antananarivo','Toliara'),'Antananarivo'),
-         ("Qui est le président de la republique à Madagascar ?",('Andry Rajoelina','Philibert Tsiranana','Donald Trump','Marc Ravalomanana'),'Andry Rajoelina')]
 
-for data in datas:Questionnaires(data[0],data[1],data[2])
+
+"""datas = [("quel est la capitale de Madagascar ?",['Fianarantsoa','Toamasina','Antananarivo','Toliara'],'Antananarivo'),
+         ("Qui est le président de la republique à Madagascar ?",['Andry Rajoelina','Philibert Tsiranana','Donald Trump','Marc Ravalomanana'],'Andry Rajoelina'),
+         ("trouvez l'intru...",['oeil','dent',"langue","peau",'oreil','nez'],'dent'),
+         ("quel esr le contraire de \"inflammable\" ?",["flammable","ininflammable","imperméable","ininflammable","incombustible"],"incombustible"),
+         ("en quel année le Stephen Hawking est décédé ?",["2018","2015","2013","2020"],"2018"),
+         ("dans quel équipe Paul Pogba n'a pas joué",["Juventus","Manchester United","Chelsea"],"Chelsea"),
+         ("laquelle de ces maladies est incurable ?",["SIDA",'Tuberculose',"Peste","Choléra","Corona virus"],"SIDA")]
+
+
+shuffle(datas)
+
+for data in datas:Questionnaires(data[0],data[1],data[2])"""
+#print(Questionnaires.count)
+Questionnaires()
 
